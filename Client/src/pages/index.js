1a4f39d487a3aa5f93291a86fe509dfd3d4e1794
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-04-23 14:35:30 -0400

    welcome section complete of home

--- Client/src/pages/index.js
+++ Client/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from "react";
+import ShapesBanner from "../assets/images/shapes-banner.svg";
 
 function Home() {
     const pageTitle = "Home";
@@ -7,10 +8,39 @@ function Home() {
         document.title = `${pageTitle} | Frim`;
     }, [pageTitle]);
     return (
-        <div>
-            <h1>{pageTitle}</h1>
-            <p>Welcome to Frim! We provide a suite of tools to help you build your startup.</p>
-        </div>
+        <main className="home">
+            <section className="welcome">
+                <div className="welcome-column">
+                    <div>
+                        <img src={ShapesBanner} alt="A triangle, a square, a circle, and a trapezoid." />
+                    </div>
+                    <div>
+                        <h1>Welcome to Frim Cloud, Francesco!</h1>
+                    </div>
+                    <div className="welcome-nav">
+                        <a href="/settings">
+                            Profile settings
+                        </a>
+                        <a href="#quick-access">
+                            Quick access
+                        </a>
+                    </div>
+                </div>
+                <div className="welcome-column">
+                    <a href="https://www.ceasar.frim.io">
+                        <div>
+                            <h2>Revitalize customer relations with Ceasar CRM</h2>
+                        </div>
+                        <div>
+                            <h2>Try Ceasar</h2>
+                            <i className="fas fa-arrow-right"></i>
+                        </div>
+                    </a>
+                </div>
+            </section>
+            <hr />
+            <section className="quick-access" id="quick-access"></section>
+        </main>
     );
 }
 
