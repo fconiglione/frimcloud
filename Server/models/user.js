77b30e8733374952a8519e52eab81ac1b8e298a2
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-07-03 22:26:58 -0400

    updated user model

--- Server/models/user.js
+++ Server/models/user.js
@@ -18,7 +18,7 @@ class User {
     async addUserToCloudDB(sub, nickname, name, picture, updated_at) {
         try {
             const values = [sub, nickname, name, picture, updated_at];
-            const query = `INSERT INTO cloud.users (sub, nickname, name, picture, updated_at) VALUES ($1, $2, $3)`;
+            const query = `INSERT INTO cloud.users (sub, nickname, name, picture, updated_at) VALUES ($1, $2, $3, $4, $5) ON CONFLICT (sub) DO UPDATE SET nickname = $2, name = $3, picture = $4, updated_at = $5 RETURNING *`;
             const result = await this.pool.query(query, values);
             return result;
         }
